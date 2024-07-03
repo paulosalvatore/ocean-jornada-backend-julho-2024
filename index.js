a1a133b7fb0650c3dc6f5f6de018b91f4a3b7fe9
Paulo Salvatore <salvatoregames@gmail.com>
2024-07-03 12:31:34 +0000

    Endpoint Read All `[GET] /item` implementado

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
 
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All - [GET] /item
+app.get('/item', function (req, res) {
+  // Pegamos a lista e enviamos como resposta HTTP
+  res.send(lista)
+})
+
 app.listen(3000)
